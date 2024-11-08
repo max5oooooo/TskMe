5bb134630fe120b8da27fd9f4dc46d28ae7a1d32
max5oooooo <maxuel5oooooo@gmail.com>
2024-11-08 20:36:54 +0100

    fixed component

--- app/src/components/TimerButtons.jsx
+++ app/src/components/TimerButtons.jsx
@@ -1,8 +1,10 @@
 
 
 import React, { useState, useEffect } from 'react';
+import useDictionary from '../hook/useDictionary';
 
 const TimeButtons = () => {
+  const dictionary = useDictionary();
   // Stati per i vari tempi
   const [startTime, setStartTime] = useState(0);
   const [breakTime, setBreakTime] = useState(0);
@@ -15,7 +17,7 @@ const TimeButtons = () => {
   // Stati per gestire lo stato dei timer
   const [isRunningStart, setIsRunningStart] = useState(false);
   const [isRunningBreak, setIsRunningBreak] = useState(false);
-  const[isCompleted, setIsCompleted] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
   const [startIntervalId, setStartIntervalId] = useState(null);
   const [breakIntervalId, setBreakIntervalId] = useState(null);
 
@@ -29,10 +31,10 @@ const TimeButtons = () => {
   };
 
   //Funzione per convertire secondi in (HH:MM)
-  const secondToTime =(totalSeconds) =>{
-    const hours = Math.floor(totalSeconds  / 3600);
-    const minutes=Math.floor((totalSeconds - hours * 3600)  / 60);
-    const seconds=(totalSeconds - hours * 3600 - minutes * 60);
+  const secondToTime = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds - hours * 3600) / 60);
+    const seconds = (totalSeconds - hours * 3600 - minutes * 60);
 
     const formattedHours = hours.toLocaleString('it-IT', {
       minimumIntegerDigits: 2,
@@ -84,7 +86,7 @@ const TimeButtons = () => {
     setIsCompleted(true);
 
     // Calcola l'Overtime
-    const predefinedTimeInSeconds = timeToSeconds(predefinedTime); 
+    const predefinedTimeInSeconds = timeToSeconds(predefinedTime);
     setOvertime(Math.max((startTime + breakTime) - predefinedTimeInSeconds, 0));
   };
 
@@ -123,33 +125,46 @@ const TimeButtons = () => {
 
   return (
     <div>
-      <h2>Gestione Timer</h2>
-      <div>
-        <label htmlFor="predefinedTime">Tempo Predefinito (hh:mm): </label>
-        <input
-          id="predefinedTime"
-          type="time"
-          value={predefinedTime}
-          onChange={handlePredefinedTimeChange}
-        />
+      <div className="flex flex-row items-center gap-5">
+        <div className=" m-1 flex flex-row items-center gap-5 rounded-md border-2  bg-slate-100" >
+          <h5>DA:</h5>
+          <label htmlFor="start" className="absolute font-medium text-dark-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-dark-600 peer-focus:dark:text-dark-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"></label>
+          <input id="start" name="date" type="date" className="block py-1.5 px-0 w-30 text-sm font-medium text-gray-900 bg-transparent border-0  appearance-none focus:outline-none focus:ring-0 " placeholder=" " />
+        </div>
+        <div className="m-1 flex flex-row items-center gap-5 rounded-md border-2 bg-slate-100 " >
+          <h5>A:</h5>
+          <label htmlFor="finish" className="absolute font-medium text-dark-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-dark-600 peer-focus:dark:text-dark-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"></label>
+          <input id="finish" name="date" type="date" className="block py-1.5 px-0 w-30 text-sm font-medium text-gray-900 bg-transparent border-0  appearance-none focus:outline-none focus:ring-0 " placeholder=" " />
+        </div>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Start Time (s)</th>
-            <th>Break Time (s)</th>
-            <th>Total Time (s)</th>
-            <th>Overtime (s)</th>
-            <th>Azione</th>
+      <table className="w-full table-fixed border-collapse border-spacing-1 mt-5 text-sm ">
+        <thead className="text-xs text-gray-700 uppercase" >
+          <tr className="bg-slate-100">
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_DAY}</th>
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK}</th>
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_START}</th>
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_BREAK}</th>
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_DEADLINE}</th>
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_TOTAL_TIME}</th>
+            <th className="border-2 bg-slate-100">Overtime</th>
+            {/*<th class="border-2 border-primary">{dictionary.TABELA_HOME.TASK_COLABORATORS}</th>*/}
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_STATUS}</th>
+            <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_PRIORITY}</th>
+            <th className="border-2 bg-slate-100">bottons</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{secondToTime(startTime)}</td>
-            <td>{secondToTime(breakTime)}</td>
-            <td>{secondToTime(totalTime)}</td>
-            <td>{overtime >= 0 ? secondToTime(overtime): 'N/A'}</td>
-            <td>
+          <tr className="text-sm text-gray-700 " >
+            <td className="border-2 border-slate-400"> </td>
+            <td className="border-2 border-slate-400"> </td>
+            <td className="border-2 border-slate-400">{secondToTime(startTime)}</td>
+            <td className="border-2 border-slate-400">{secondToTime(breakTime)}</td>
+            <td className="border-2 border-slate-400"> </td>
+            <td className="border-2 border-slate-400">{secondToTime(totalTime)}</td>
+            <td className="border-2 border-slate-400">{overtime >= 0 ? secondToTime(overtime) : 'N/A'}</td>
+            <td className="border-2 border-slate-400"></td>
+            <td className="border-2 border-slate-400"></td>
+            <td className="border-2 border-slate-400">
               <button onClick={toggleStartStop} disabled={isCompleted}>
                 {isRunningStart ? 'Stop' : 'Start'}
               </button>
--- app/src/pages/console/Home.jsx
+++ app/src/pages/console/Home.jsx
@@ -16,60 +16,7 @@ const HomeConsole = ({ form }) => {
             </div>
             <div>
                 <ConsoleContentBox>
-                    <div>
-                        <div className="flex flex-row items-center gap-5">
-                            <div className=" m-1 flex flex-row items-center gap-5 rounded-md border-2  bg-slate-100" >
-                                <h5>DA:</h5>
-                                <label htmlFor="start" className="absolute font-medium text-dark-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-dark-600 peer-focus:dark:text-dark-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"></label>
-                                <input id="start" name="date" type="date" className="block py-1.5 px-0 w-30 text-sm font-medium text-gray-900 bg-transparent border-0  appearance-none focus:outline-none focus:ring-0 " placeholder=" " />
-                            </div>
-                            <div className="m-1 flex flex-row items-center gap-5 rounded-md border-2 bg-slate-100 " >
-                                <h5>A:</h5>
-                                <label htmlFor="finish" className="absolute font-medium text-dark-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-dark-600 peer-focus:dark:text-dark-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"></label>
-                                <input id="finish" name="date" type="date" className="block py-1.5 px-0 w-30 text-sm font-medium text-gray-900 bg-transparent border-0  appearance-none focus:outline-none focus:ring-0 " placeholder=" " />
-                            </div>
-                        </div>
-                    </div>
-                        <table className="w-full table-fixed border-collapse border-spacing-1 mt-5 text-sm ">
-                            <thead className="text-xs text-gray-700 uppercase" >
-                                <tr className="bg-slate-100">
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_DAY}</th>
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK}</th>
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_START}</th>
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_BREAK}</th>
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_DEADLINE}</th>
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_TOTAL_TIME}</th>
-                                    <th className="border-2 bg-slate-100">Overtime</th>
-                                    {/*<th class="border-2 border-primary">{dictionary.TABELA_HOME.TASK_COLABORATORS}</th>*/}
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_STATUS}</th>
-                                    <th className="border-2 bg-slate-100">{dictionary.TABELA_HOME.TASK_PRIORITY}</th>
-                                    <th className="border-2 bg-slate-100">bottons</th>
-                                </tr>
-                            </thead>
-                            <tbody className=" bg-white ">
-                                {tasks.map((task) => (
-
-                                <tr className="text-sm text-gray-700 " >
-                                    <td className="border-2 border-slate-400">{task.date} Ciao</td>
-                                    <td className="border-2 border-slate-400">{task.title} come</td>
-                                    <td className="border-2 border-slate-400 "> bla</td>
-                                    <td className="border-2  border-slate-400 "></td>
-                                    <td className="border-2  border-slate-400">{task.time}</td>
-                                    <td className="border-2 border-slate-400"></td>
-                                    <td className="border-2 border-slate-400"></td>
-                                    <td className="border-2 border-slate-400"></td>
-                                    <td className="border-2 border-slate-400">{task.priority}</td>
-                                    <td className="border-2 border-slate-400">
-                                    </td>
-                                </tr>
-                               
-                            ))}
-                               
-                            </tbody>
-                        </table>
                         < TimerButtons />
-
-                    
                 </ConsoleContentBox>
             </div>
         </>
