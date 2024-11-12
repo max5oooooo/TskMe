5556acae4707c83575aaf1afffe82b3b3f476f31
alexdant91 <alexdant91@gmail.com>
2024-11-12 16:17:51 +0100

    forced auth state

--- app/src/store/slices/authSlice.js
+++ app/src/store/slices/authSlice.js
@@ -4,9 +4,25 @@ const auth = JSON.parse(localStorage.getItem("auth"));
 
 const authSlice = createSlice({
     name: "auth",
-    initialState: {
+    /*initialState: {
         token: auth?.token || null,
         user: auth?.user || null
+    },*/
+    initialState: {
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MzFjN2Q5NTU3MTdkNmEyYTc1YzcyOSIsImVtYWlsIjoiYWxleGRhbnQ5MUBnbWFpbC5jb20iLCJyb2xlcyI6WyJvd25lciJdLCJpYXQiOjE3MzE0MjQ2MjB9.lfwNFgVCX_cKiPG3QrnQoRpHV8u-MRgRG2zDMOV4b68',
+        user: {
+            _id: '6731c7d955717d6a2a75c729',
+            first_name: 'Alessandro',
+            last_name: 'D\'Antoni',
+            email: 'alexdant91@gmail.com',
+            roles: [
+                'owner'
+            ],
+            profile_image: null,
+            createdAt: '2024-11-11T09:01:13.060Z',
+            updatedAt: '2024-11-11T10:05:29.869Z',
+            description: 'Test della mia descrizione nel database'
+        }
     },
     reducers: {
         login: (state, { payload }) => {
